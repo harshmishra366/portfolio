7ac8c8fbf0bc5fbe26de48f6b12d90d21e9b4742
harshmishra366 <mishraharsh5959@gmail.com>
2025-07-09 20:21:46 +0530

    style: format code for improved readability in Footer component

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import Link from 'next/link';
+import React from "react";
+import Link from "next/link";
 
 function Footer() {
   return (
@@ -7,21 +7,23 @@ function Footer() {
       <div className="container mx-auto flex flex-col md:flex-row justify-between items-center">
         <div className="mb-4 md:mb-0">
           <h3 className="text-lg font-bold">Harsh Mishra</h3>
-          <p className="text-sm">Aspiring Software Engineer & Full-Stack Developer</p>
+          <p className="text-sm">
+            Aspiring Software Engineer & Full-Stack Developer
+          </p>
         </div>
         <div className="flex space-x-4">
-          <Link href={'https://github.com/harshmishra366'} target='blank'>
-          Github
+          <Link href={"https://github.com/harshmishra366"} target="blank">
+            Github
           </Link>
-          <Link href={'https://github.com/harshmishra366'} target='blank'>
-          LinkedIn
+          <Link href={"https://www.linkedin.com/in/harsh--mishra?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app"} target="blank">
+            LinkedIn
           </Link>
-          <Link href={'https://github.com/harshmishra366'} target='blank'>
-          Instagram
-          </Link>
+          <a href="mailto:mishraharsh5959@gmail.com">Email me</a>
         </div>
         <div className="mt-4 md:mt-0">
-          <p className="text-sm">&copy; {new Date().getFullYear()} Harsh Mishra. All rights reserved.</p>
+          <p className="text-sm">
+            &copy; {new Date().getFullYear()} Harsh Mishra. All rights reserved.
+          </p>
         </div>
       </div>
     </footer>
